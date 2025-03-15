feff30919dae56d9261ea8bdc7d5d1dd91409df6
Yihan Ding <yihand6@uci.edu>
2025-03-15 21:02:43 +0000

    w

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -42,11 +42,9 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
     reverse(path.begin(), path.end());
     return path;
 }
-
 void print_path(const vector<int>& path, int total) {
     for (size_t i = 0; i < path.size(); ++i) {
-        cout << path[i];
-        if (i < path.size() - 1) cout << " ";
+        cout << path[i] << " "; 
     }
-    cout << " \nTotal cost is " << total << endl;
+    cout << "\nTotal cost is " << total << endl;
 }
